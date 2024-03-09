d32b4147e9c35f07e52da35f02b93d8dedeb8d30
Andres Prieto <andresprieto2703@gmail.com>
2024-03-08 23:47:04 -0500

    Utils
    Adding dirs management also adding a new env var

--- task/lab_1/util/env_vars.py
+++ task/lab_1/util/env_vars.py
@@ -3,11 +3,13 @@ from pathlib import Path
 from dotenv import load_dotenv
 from os.path import join, dirname 
 
+
 project_path = Path(dirname(__file__))
 env_file = join(project_path.parent.absolute(), ".env")
 
 load_dotenv(env_file)
 
 config = {
-    "datasets": os.environ.get("DATASET_DIR")
+    "datasets": os.environ.get("DATASET_DIR"),
+    "save": os.environ.get("SAVE_DIR")
 }
