dc410001aca51c69d36562105bc051fb32f0f61a
Andres M. Prieto <andresprieto2703@gmail.com>
2024-03-12 06:10:00 -0500

    Tracking train & test size env vars

--- task/lab_1/util/env_vars.py
+++ task/lab_1/util/env_vars.py
@@ -1,7 +1,7 @@
 import os
 from pathlib import Path
 from dotenv import load_dotenv
-from os.path import join, dirname 
+from os.path import join, dirname
 
 
 project_path = Path(dirname(__file__))
@@ -13,4 +13,7 @@ config = {
     "datasets": os.environ.get("DATASET_DIR"),
     "save": os.environ.get("SAVE_DIR"),
     "stage": os.environ.get("STAGE"),
+
+    "train_size": float(os.environ.get("TRAIN_SIZE")),
+    "test_size": float(os.environ.get("TEST_SIZE")),
 }
