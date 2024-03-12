75fb6521d105b9965e25c3ce033b99b4d587e6d1
Andres M. Prieto <andresprieto2703@gmail.com>
2024-03-12 04:03:22 -0500

    Research #1
    Moving research to another folder

--- task/lab_1/main.py
+++ task/lab_1/main.py
@@ -1,36 +1,24 @@
 import time
-import pandas as pd
-from load.main import load_dataset
-from preprocessing.main import clear_data
-from analysis.plotting import heat, scatter
-from analysis.main import correlation
+from util.env_vars import config
+from research.main import research_1
 
 
-# To track data
-timestamp = time.time()
+research = {
+    "1": research_1,
+}
 
-dataset = load_dataset("Popular_Spotify_Songs.csv")
-dataset = clear_data(
-    dataset, [0, 1, 3, 4, 5, 6, 7, 9, 10, 11, 12, 13, 15, 16, 18, 20, 21, 22]
-)
 
-# Just fixing 'streams' column (It's str by default)
-dataset["streams"] = pd.to_numeric(dataset["streams"], errors="coerce")
+def select_stage(stage):
+    return research.get(stage, "Error")
 
-"""
-- Correlation BPM and Streams
-- Correlation Artist count and Streams
-- Correlation Speechiness count and Streams
-- Correlation Danceability and Streams
-- Correlation Danceability count and Energy
-"""
 
-correlation = correlation(dataset.copy(deep=True), [1, 1, 1, 1, 1, 1])
+if __name__ == '__main__':
+    # To track data
+    timestamp = time.time()
 
-# heat(correlation, title="Correlation heat map", id=f"{timestamp}", s=True)
+    research = select_stage(config["stage"])
 
-# scatter([dataset["bpm"], dataset["streams"]], "Streams vs BPM", id=f"{timestamp}", x="BPM", y="Streams", s=True)
-# scatter([dataset["artist_count"], dataset["streams"]], "Streams vs Artiste count", id=f"{timestamp}", x="Artist count", y="Streams", s=True)
-# scatter([dataset["speechiness_%"], dataset["streams"]], "Streams vs Speechiness (%)", id=f"{timestamp}", x="Speechiness", y="Streams", s=True)
-# scatter([dataset["danceability_%"], dataset["streams"]], "Streams vs Danceability (%)", id=f"{timestamp}", x="Danceability", y="Streams", s=True)
-# scatter([dataset["danceability_%"], dataset["energy_%"]], "Energy (%) vs Danceability (%)", id=f"{timestamp}", x="Danceability", y="Energy", s=True)
+    if research == "Error":
+        raise Exception("There is an error on stage definition")
+
+    research(timestamp)
