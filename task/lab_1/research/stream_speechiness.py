97e042663123107e387f5c103415ca40aa3d738d
Andres Prieto <andresprieto2703@gmail.com>
2024-03-13 03:23:50 -0500

    Lintting and plotting
    Adding a plotting function from analysis/plotting.py also litting this
    file

--- task/lab_1/research/stream_speechiness.py
+++ task/lab_1/research/stream_speechiness.py
@@ -1,6 +1,6 @@
-import pandas as pd
 from load.main import load_dataset
-from preprocessing.main import clear_data
+from preprocessing.main import clear_data, get_features
+from analysis.plotting import plt_regression
 from analysis.main import get_regression
 
 
@@ -16,12 +16,45 @@ def predict(timestamp: float) -> None:
     """
 
     dataset = load_dataset("Popular_Spotify_Songs.csv")
-    unused = [0, 1, 2, 3, 4, 5, 6, 7, 9, 10, 11,
-              12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22]
+
+    unused = [
+        0,  # Track name
+        1,  # Artist(s) name
+        2,  # Artist count
+        3,  # Released year
+        4,  # Released month
+        5,  # Released day
+        6,  # In Spotify playlist
+        7,  # In Spotify charts
+        # 8,  # Streams
+        9,  # In apple playlist
+        10,  # In apple charts
+        11,  # In Deezer playlists
+        12,  # In Deezer charts
+        13,  # In Shazam charts
+        14,  # BPM
+        15,  # Key
+        16,  # Mode
+        17,  # Danceability (%)
+        18,  # Valence (%)
+        19,  # Energy (%)
+        20,  # Acousticness (%)
+        21,  # Instrumentalness (%)
+        22,  # Liveness (%)
+        # 23,  # Speechiness (%)
+    ]
     dataset = clear_data(dataset=dataset, columns_to_drop=unused)
+    features, target = get_features(dataset["speechiness_%"], dataset["streams"])
 
-    # Just fixing 'streams' column (It's str by default)
-    dataset["streams"] = pd.to_numeric(dataset["streams"], errors="coerce")
+    linear_regression = get_regression(
+        regression_type="linear", data=features, target=target, test=True
+    )
 
-    get_regression(regression_type="linear",
-                   data=dataset["speechiness_%"], target=dataset["streams"])
+    plt_regression(
+        linear_regression.predict(features),
+        features,
+        target,
+        id=timestamp,
+        x="Features",
+        y="Label",
+    )
