b0f7f904776d4f3650e660f76e1c9920e5cf7074
Andres Prieto <andresprieto2703@gmail.com>
2024-03-15 05:53:07 -0500

    NN
    Adding neuronal network

--- task/lab_1/analysis/main.py
+++ task/lab_1/analysis/main.py
@@ -1,6 +1,9 @@
 import pandas as pd
-from typing import List
+from typing import Any, List, Tuple, Union, Optional
+
+from tensorflow.keras.models import Sequential
 from sklearn.linear_model import LinearRegression
+from analysis.neuronal_network import create_nn, train_nn
 from analysis.regression import linear_regression, test_regression
 from analysis.normalization import simple_scaling, min_max, z_score
 
@@ -49,3 +52,40 @@ def get_regression(
         test_regression(regression, x_test, y_test)
 
     return regression
+
+
+def get_nn(
+    input_layer: int,
+    hidden_layer: List[List[Union[int, str, Optional[int]]]],
+    output_layer: int,
+    data: pd.DataFrame,
+    target: pd.Series,
+    epochs: int,
+) -> Tuple[Sequential, Sequential, list, list, list, list]:
+    """Create a trained neuronal network
+
+    Args:
+        input_layer (int): Defines NN inputs
+        hidden_layer (List[List[Union[int, str, Optional[int]]]]): Defines hidden layers of NN as [[units, activation_function, dropout (Optionally)]]. The activation_function must be any of the following functions:
+        "relu", "sigmoid", "softmax", "softplus","softsign", "tanh", "selu", "elu", "exponential", "leaky_relu", "relu6", "silu", "hard_silu", "gelu", "hard_sigmoid", "linear", "mish" or "log_softmax"
+        output_layer (int): Define the output layer
+        data (pd.DataFrame): A dataset as a pd.DataFrame
+        target (pd.Series): A series of targets
+        epochs (int): Training epochs
+
+    Returns:
+        Sequential: A trained neuronal network
+        train_log: Neuronal network history
+        list: x_test
+        list: y_test
+        list: x_train
+        list: y_train
+    """
+    nn = create_nn(
+        input_layer=input_layer, hidden_layer=hidden_layer, output_layer=output_layer
+    )
+    nn, train_log, x_test, y_test, x_train, y_train = train_nn(
+        neuronal_network=nn, data=data, target=target, epochs=epochs
+    )
+
+    return nn, train_log, x_test, y_test, x_train, y_train
