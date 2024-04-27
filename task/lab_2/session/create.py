aff787b330905a5494827f02853fb31d91a57203
Andres M. Prieto <andresprieto2703@gmail.com>
2024-04-27 08:04:40 -0500

    Session
    Using config from SparkSession

--- task/lab_2/session/create.py
+++ task/lab_2/session/create.py
@@ -1,5 +1,6 @@
 import findspark
 from pyspark.sql import SparkSession
+from util.env_vars import config
 
 findspark.init()
 
@@ -7,12 +8,11 @@ findspark.init()
 def create_session():
     spark = (
         SparkSession.builder.appName("SparkSession")
-        .config("spark.master", "local[4]")
-        .config("spark.executor.memory", "1g")
-        .config("spark.sql.shuffle.partitions", 1)
-        .config("spark.driver.memory", "1g")
+        .config("spark.master", f"local[{config['spark_cores']}]")
+        .config("spark.executor.memory", f"{config['spark_exec_memory']}g")
+        .config("spark.sql.shuffle.partitions", config['spark_shuffle'])
+        .config("spark.driver.memory", f"{config['spark_drive_memory']}g")
         .getOrCreate()
     )
 
     return spark
-
