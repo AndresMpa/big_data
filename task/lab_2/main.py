f0669ce53e2cac7752199f1c8a8f8d04eca4f95f
Andres M. Prieto <andresprieto2703@gmail.com>
2024-04-26 23:58:57 -0500

    Moving main into usable structure
    Adding a better structure to handle with error and sending notifications
    through notify-send for dbust (On Linux)

--- task/lab_2/main.py
+++ task/lab_2/main.py
@@ -1,8 +1,39 @@
 import time
+import subprocess
+from session.create import create_session
+from load.main import get_dataset, adjust_string_columns, adjust_num_columns
+from experiment.main import experimental_case
+from util.logger import trace_log
 from util.env_vars import config
 
 if __name__ == '__main__':
     timestamp = str(time.time())
-    
-    print(config)
-    print(timestamp)
+
+    try:
+        spark_session = create_session()
+
+        dataset = get_dataset(
+            spark_session, "final_animedataset.csv", ",", columns_to_drop=["username", "anime_id", "my_score", "user_id", "title"])
+        dataset = adjust_num_columns(
+            dataset, ["score", "scored_by", "rank", "popularity"])
+        dataset, log = adjust_string_columns(
+            dataset, ["gender", "type", "source"])
+        trace_log(timestamp, log)
+        dataset = dataset.dropna()
+
+        experimental_case(dataset)
+
+        dataset.show()
+
+        spark_session.stop()
+
+        subprocess.run(
+            ['notify-send',
+             'Process ended',
+             'General process has ended, check results.'])
+
+    except Exception as e:
+        subprocess.run(
+            ['notify-send',
+             "An error just happened",
+             'An error interrupeted the script execution'])
